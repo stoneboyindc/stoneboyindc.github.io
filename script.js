b82f92231a2bdb33c3782a0085aad00d57699022
coro121 <courtneyb.roberson@gmail.com>
2019-08-06 19:57:06 -0400

    trying to merge

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
+'use strict'
 
-const geoAPI = "https://api.mapbox.com/geocoding/v5/mapbox.places";
+//const geoAPI = "https://api.mapbox.com/geocoding/v5/mapbox.places";
 
-const mpKey = `pk.eyJ1IjoiY2FybG9ydHkiLCJhIjoiY2p5eGtsemNjMGRhZTNncWV6am5xYXN1ZCJ9.FmCOZr6penXoMtoJuThUog`;  // <== remember to pluck in your MapBox API Key
+//const mpKey = `pk.eyJ1IjoiY2FybG9ydHkiLCJhIjoiY2p5eGtsemNjMGRhZTNncWV6am5xYXN1ZCJ9.FmCOZr6penXoMtoJuThUog`;  // <== remember to pluck in your MapBox API Key
 
-const mbToken = `.json?access_token=${mpKey}`
+//const mbToken = `.json?access_token=${mpKey}`
 
-const iSSurl = 'http://api.open-notify.org/iss-pass.json?'
+//const iSSurl = 'http://api.open-notify.org/iss-pass.json?'
 
-fetch(`${geoAPI}/New York${mbToken}`).then(resp => {
+const url = "http://api.open-notify.org/iss-pass.json?lat=45&lon=45"
+const proxyURL = "https://cors-anywhere.herokuapp.com/"
+
+function getLocation() {
+	const options = {
+		headers: header("Access-Control-Allow-Origin: *")
+		}
+	};
+
+	fetch(proxyUrl, url)
+		.then(response => response.json())
+		.then(responseJson => console.log(responseJson));
+
+
+$(getLocation)
+
+
+/*fetch(`${geoAPI}/New York${mbToken}`).then(resp => {
     if(resp.ok) {
         return resp.json();
     }
@@ -27,4 +45,17 @@ fetch(`${geoAPI}/New York${mbToken}`).then(resp => {
     
 }).catch(errM => {
     console.log('Something wrong with MapBox', errM);
-})
+})*/
+
+/*function watchForm() {
+	$("form").submit(event => {
+		console.log("Calculating...");
+		event.preventDefault();
+
+		//getLocation();
+	})/*
+
+$(function () {
+	console.log("Ready for your input");
+	watchForm();
+});*/
