b3ffe87d597bd68d36ee1e060dde487f0e308329
coro121 <courtneyb.roberson@gmail.com>
2019-08-06 22:14:08 -0400

    changes at 22:15

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const geoAPI = "https://api.mapbox.com/geocoding/v5/mapbox.places";
 
-const mpKey = `pk.eyJ1IjoiY2FybG9ydHkiLCJhIjoiY2p5eGtsemNjMGRhZTNncWV6am5xYXN1ZCJ9.FmCOZr6penXoMtoJuThUog`;  // <== remember to pluck in your MapBox API Key
+const mpKey = `pk.eyJ1IjoiY2FybG9ydHkiLCJhIjoiY2p6MGpxNmNsMGNkYzNncWloeGtjdWc0MiJ9.44uitu32EXkSSvz3FWF0FA`;  // <== remember to pluck in your MapBox API Key
 
 const mbToken = `.json?access_token=${mpKey}`
 
@@ -10,37 +10,53 @@ let proxyUrl = 'https://cors-anywhere.herokuapp.com/'
 
 const iSSurl = 'http://api.open-notify.org/iss-pass.json?'
 
-/*fetch(`${geoAPI}/New York${mbToken}`).then(resp => {
-    if(resp.ok) {
-        return resp.json();
-    }
-}).then(resp => {
-    const [lon, lat] = resp.features[0].center;
+function getLocation() {
+	fetch(`${geoAPI}/${userInput()}${mbToken}`).then(response => {
+		if (response.ok) {
+			return response.json();
+		}
+	}).then(response => {
+		const [lon, lat] = response.features[0].center;
 
-		fetch(proxyUrl + `${iSSurl}lat=${lat}&lon=${lon}`).then(res=>{
-		    if(res.ok) {
-		        return res.json()
-		    }
+		fetch(proxyUrl + `${iSSurl}lat=${lat}&lon=${lon}`).then(res => {
+			if (res.ok) {
+				return res.json()
+			}
 		}).then(res => {
-		    console.log(res);
-		    // do something with the response from ISS
+			console.log(res);
+			// do something with the response from ISS
 		}).catch(errI => {
-		    console.log('Something wrong with ISS', errI);
+			console.log('Something wrong with ISS', errI);
 		})
 
-}).catch(errM => {
-    console.log('Something wrong with MapBox', errM);
-})*/
+	}).catch(errM => {
+		console.log('Something wrong with MapBox', errM);
+	})
+}
+
+function displayResults(responseJson) {
+	let data = ['response'].forEach(function(d) {
+		let date = new Date(d['risetime'] * 1000);
+		$('#pass-results').append('<li>' + date.toString() + '</li>');
+		//return data;
+	});
+};
+
+function userInput() {
+	let userLocation = $("#address").val();
+	return userLocation;
+}
 
 function watchForm() {
 	$("form").submit(event => {
 		console.log("Calculating...");
 		event.preventDefault();
+		getLocation(userInput);
+	});
+}
 
-		//getLocation();
-	})
-
-$(function () {
+$(function() {
 	console.log("Ready for your input");
 	watchForm();
+	displayResults();
 })
