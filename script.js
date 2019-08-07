0507b5a6413710fdd61137949f9851d8f8198b7e
Carl Ortutay <carlorty@gmail.com>
2019-08-06 23:21:24 -0400

    updates to js file. app now functional. added an empty method to display function to clear previous results when calling a new search

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const geoAPI = "https://api.mapbox.com/geocoding/v5/mapbox.places";
 
-const mpKey = `pk.eyJ1IjoiY2FybG9ydHkiLCJhIjoiY2p6MGpxNmNsMGNkYzNncWloeGtjdWc0MiJ9.44uitu32EXkSSvz3FWF0FA`;  // <== remember to pluck in your MapBox API Key
+const mpKey = `pk.eyJ1IjoiY2FybG9ydHkiLCJhIjoiY2p6MGpxNmNsMGNkYzNncWloeGtjdWc0MiJ9.44uitu32EXkSSvz3FWF0FA`;
 
 const mbToken = `.json?access_token=${mpKey}`
 
@@ -11,20 +11,21 @@ let proxyUrl = 'https://cors-anywhere.herokuapp.com/'
 const iSSurl = 'http://api.open-notify.org/iss-pass.json?'
 
 function getLocation() {
-	fetch(`${geoAPI}/${userInput()}${mbToken}`).then(response => {
+	let passes = $("#num").val();
+  fetch(`${geoAPI}/${userInput()}${mbToken}`).then(response => {
 		if (response.ok) {
 			return response.json();
 		}
 	}).then(response => {
 		const [lon, lat] = response.features[0].center;
 
-		fetch(proxyUrl + `${iSSurl}lat=${lat}&lon=${lon}`).then(res => {
+		fetch(proxyUrl + `${iSSurl}lat=${lat}&lon=${lon}&n=${passes}`).then(res => {
 			if (res.ok) {
 				return res.json()
 			}
 		}).then(res => {
 			console.log(res);
-			// do something with the response from ISS
+			displayResults(res);
 		}).catch(errI => {
 			console.log('Something wrong with ISS', errI);
 		})
@@ -35,10 +36,10 @@ function getLocation() {
 }
 
 function displayResults(responseJson) {
-	let data = ['response'].forEach(function(d) {
+  $('#pass-results').empty();
+  responseJson.response.forEach(function(d) {
 		let date = new Date(d['risetime'] * 1000);
 		$('#pass-results').append('<li>' + date.toString() + '</li>');
-		//return data;
 	});
 };
 
@@ -51,12 +52,11 @@ function watchForm() {
 	$("form").submit(event => {
 		console.log("Calculating...");
 		event.preventDefault();
-		getLocation(userInput);
+		getLocation();
 	});
 }
 
 $(function() {
 	console.log("Ready for your input");
 	watchForm();
-	displayResults();
 })
